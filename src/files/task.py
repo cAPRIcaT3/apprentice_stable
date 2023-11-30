ca29d0937124d4d081b8c7f1633f357077727a38
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-30 14:43:48 +0530

    Update task.py

--- src/files/task.py
+++ src/files/task.py
@@ -3,8 +3,11 @@
 def calculate_sum(a, b):
     return a + b
 
-if __name__ == "__main__":
+def main():
     num1 = 5
     num2 = 10
     result = calculate_sum(num1, num2)
     print(f"The sum of {num1} and {num2} is: {result}")
+
+if __name__ == "__main__":
+    main()
