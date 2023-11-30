b173a3702dd586c1daa885d1564da5f2ee2af820
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-30 14:37:16 +0530

    Create task.py

---
+++ src/files/task.py
@@ -0,0 +1,10 @@
+# specialized_task.py
+
+def calculate_sum(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = 5
+    num2 = 10
+    result = calculate_sum(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {result}")
